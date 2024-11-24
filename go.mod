e412eb11c0bcaf56b3fae96f1449418e9445bbe6
Phạm Huỳnh Đăng Khoa <khoapham1405@gmail.com>
2024-11-24 15:29:12 +0700

    feat 19: migrating schema with gorm to mysql and dump database

--- go.mod
+++ go.mod
@@ -57,16 +57,20 @@ require (
 	golang.org/x/arch v0.12.0 // indirect
 	golang.org/x/crypto v0.29.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
-	golang.org/x/mod v0.17.0 // indirect
+	golang.org/x/mod v0.22.0 // indirect
 	golang.org/x/net v0.31.0 // indirect
 	golang.org/x/sync v0.9.0 // indirect
 	golang.org/x/sys v0.27.0 // indirect
 	golang.org/x/text v0.20.0 // indirect
-	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
+	golang.org/x/tools v0.27.0 // indirect
 	google.golang.org/protobuf v1.35.2 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/datatypes v1.2.4 // indirect
 	gorm.io/driver/mysql v1.5.7 // indirect
 	gorm.io/driver/sqlite v1.5.6 // indirect
+	gorm.io/gen v0.3.26 // indirect
 	gorm.io/gorm v1.25.12 // indirect
+	gorm.io/hints v1.1.2 // indirect
+	gorm.io/plugin/dbresolver v1.5.3 // indirect
 )
--- internal/initialize/mysql.go
+++ internal/initialize/mysql.go
@@ -5,9 +5,10 @@ import (
 	"time"
 
 	"github.com/shinkaym/go-ecommerce-backend-api/global"
-	"github.com/shinkaym/go-ecommerce-backend-api/internal/po"
+	"github.com/shinkaym/go-ecommerce-backend-api/internal/model"
 	"go.uber.org/zap"
 	"gorm.io/driver/mysql"
+	"gorm.io/gen"
 	"gorm.io/gorm"
 )
 
@@ -31,6 +32,7 @@ func InitMysql() {
 
 	// set Pool
 	SetPool()
+	genTableDAO()
 	migrateTables()
 }
 
@@ -45,10 +47,30 @@ func SetPool() {
 	sqlDb.SetConnMaxLifetime(time.Duration(m.ConnMaxLifeTime))
 }
 
+// mysql to model
+func genTableDAO() {
+	// Initiate the tables
+	g := gen.NewGenerator(gen.Config{
+		OutPath: "./internal/model",
+		Mode:    gen.WithoutContext | gen.WithDefaultQuery | gen.WithQueryInterface, // generate mode
+	})
+	// gormdb, _ := gorm.Open(mysql.Open("root:@(127.0.0.1:3306)/demo?charset=utf8mb4&parseTime=True&loc=Local"))
+	g.UseDB(global.Mdb) // reuse your gorm db
+	g.GenerateModel("go_crm_user")
+	// g.GenerateAllTable()
+	// // Generate basic type-safe DAO API for struct `model.User` following conventions
+	// g.ApplyBasic(model.User{})
+	// // Generate Type Safe API with Dynamic SQL defined on Querier interface for `model.User` and `model.Company`
+	// g.ApplyInterface(func(Querier) {}, model.User{}, model.Company{})
+	// Generate the code
+	g.Execute()
+}
+
 func migrateTables() {
 	err := global.Mdb.AutoMigrate(
-		&po.User{},
-		&po.Role{},
+		// &po.User{},
+		// &po.Role{},
+		&model.GoCrmUserV2{},
 	)
 	if err != nil {
 		fmt.Println("Migration tables error: ", err)
